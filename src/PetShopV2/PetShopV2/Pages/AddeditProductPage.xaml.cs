487273cae6205178dae458232422e4bf1f1d2f0d
gogen2424 <124147260+gogen2424@users.noreply.github.com>
2024-09-17 19:53:03 +0300

    Initial commit

---
+++ src/PetShopV2/PetShopV2/Pages/AddeditProductPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetShopV2.Pages
+{
+
+    /// <summary>
+    /// Логика взаимодействия для AddeditProductPage.xaml
+    /// </summary>
+    public partial class AddeditProductPage : Page
+    {
+        public string FlagAddOrEdit = "default";
+        public Data.Product _currentProduct = new Data.Product();
+
+
+
+        public AddeditProductPage(Data.Product product)
+        {
+            InitializeComponent();
+            if (product != null)
+            {
+                _currentProduct = product;
+                FlagAddOrEdit = "elit";
+
+            }
+            else
+            {
+                FlagAddOrEdit = "Add";
+
+            }
+
+            DataContext = _currentProduct;
+
+            Init();
+        }
+
+        public void Init()
+        {
+            try
+            {
+                CategoryComboBox.ItemsSource = Data.Stepanyan_GeorgyEntities.GetContext().Category.ToList();
+                
+                if(FlagAddOrEdit == "add")
+                {
+                    IdTextBox.Visibility = Visibility.Hidden;
+                    IdLabel.Visibility = Visibility.Hidden;
+
+                    CategoryComboBox.SelectedItem = null;
+                    CountTextBox.Text = string.Empty;
+                    UnitTextBox.Text = string.Empty;
+                    NameTextBox.Text = string.Empty;
+                    CostTextBox.Text = string.Empty;
+                    SupplierTextBox.Text = string.Empty;
+                    DescriptionTextBox.Text = string.Empty;
+                   
+                }
+                else if (FlagAddOrEdit == "edit")
+                {
+
+                    IdTextBox.Visibility = Visibility.Visible;
+                    IdLabel.Visibility = Visibility.Visible;
+
+
+                    CategoryComboBox.SelectedItem = null;
+                    CountTextBox.Text = _currentProduct.ProductQuantityInStock.ToString();
+                    UnitTextBox.Text = _currentProduct.Units.NameOfUnit;
+                    NameTextBox.Text = _currentProduct.ProductName.Name;
+                    CostTextBox.Text = _currentProduct.ProductCost.ToString();
+                    SupplierTextBox.Text = _currentProduct.Supplier.Name;
+                    DescriptionTextBox.Text = _currentProduct.ProductDesription;
+
+
+
+                }
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/PetShopV2/PetShopV2/Pages/LoginPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PetShopV2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                if (string.IsNullOrEmpty(LoginTextBox.Text))
+                {
+                    errors.AppendLine("Заполните логин");
+                }
+                if (string.IsNullOrEmpty(PasswordBox.Password))
+                {
+                    errors.AppendLine("Заполните пароль");
+                }
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (Data.Stepanyan_GeorgyEntities.GetContext().User
+                    .Any(d => d.UserLogin == LoginTextBox.Text
+                    && d.UserPassword == PasswordBox.Password))
+                {
+                    var user = Data.Stepanyan_GeorgyEntities.GetContext().User
+                        .Where(d => d.UserLogin == LoginTextBox.Text
+                        && d.UserPassword == PasswordBox.Password).FirstOrDefault();
+                    switch (user.Role.RoleName)
+                    {
+                        case "Администратор":
+                            Classes.Manager.MainFrame.Navigate(new Pages.AddeditProductPage());
+                            break;
+                        case "Клиент":
+                            Classes.Manager.MainFrame.Navigate(new Pages.ViewProductsPage());
+                            break;
+                        case "Менеджер":
+                            Classes.Manager.MainFrame.Navigate(new Pages.ViewProductsPage());
+                            break;
+                    }
+
+                    MessageBox.Show("Успех!", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный логин/пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+    }
+}
